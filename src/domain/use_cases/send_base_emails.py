93736a2a1108ad9974b3ccfa84791ea1e38322d8
erick.empresa <erickcelestimo@gmail.com>
2025-05-14 16:01:33 -0300

    feat: add transform base metod in send base emails class

--- src/domain/use_cases/send_base_emails.py
+++ src/domain/use_cases/send_base_emails.py
@@ -36,6 +36,19 @@ class SendBaseEmailsManager:
                 print(f'Coletando dados da planilha: {row['path']}')
                 self.references = pd.read_excel(row['path'])
 
+    def transform_base(self):
+        columns = ['ID', 'ID_Gerencia', 'ID_Gestor', 'Email_VP', 'Email_Gestor', 'Email_Regional', 'Gerente_Financeiro', 'Diretor_Financeiro', 'Marca', 'Filial', 'Conta', 'Verba', 'Centro_De_Custo', 'BU', 'CD_Planilha', 'VP', 'Descricao_VP', 'Grupo', 'Email', 'Email_Ferias', 'Data_Retorno_Ferias', 'De_Ferias']
+        
+        df_complete_base = self.references.merge(self.managers_list, left_on='ID_Gestor', right_on='ID', suffixes=('', '_managers'))
+        df_complete_base = df_complete_base.merge(self.budget_sets, left_on='ID_Gerencia', right_on='ID', suffixes=('', '_sets'))
+        
+        df_complete_base = df_complete_base[columns]
+        df_complete_base['De_Ferias'] = df_complete_base['De_Ferias'].map({True: 'Sim', False: 'Não'})
+        df_complete_base = df_complete_base.rename(columns={'ID_Gerencia': 'ID_Conjunto'})
+        
+        df_complete_base.to_excel("Acessos_PCO_Base_Completa.xlsx", index=False)
+
     def run(self):
-        #self.update_lists()
+        self.update_lists()
         self.read_bases()
+        self.transform_base()
