210ec5cca3261247c6741bcc41331ce1c7374564
erick.empresa <erickcelestimo@gmail.com>
2025-05-14 14:21:38 -0300

    feat: add read bases metod in send base emails class

--- src/domain/use_cases/send_base_emails.py
+++ src/domain/use_cases/send_base_emails.py
@@ -1,18 +1,41 @@
+import pandas as pd
 from data_access import UpdateExcelList
 
 class SendBaseEmailsManager:
     def __init__(self):
         self.list_to_update = [
-            r'C:\Dev\Scripts\Python\python-tools\excel\PCO_Gestores.xlsx',
-            r'C:\Dev\Scripts\Python\python-tools\excel\PCO_Conjuntos.xlsx',
-            r'C:\Dev\Scripts\Python\python-tools\excel\PCO_Referencias.xlsx'
+            {
+                'id': 1,
+                'path': r'C:\Dev\Scripts\Python\python-tools\excel\PCO_Gestores.xlsx'
+            },
+            {
+                'id': 2,
+                'path': r'C:\Dev\Scripts\Python\python-tools\excel\PCO_Conjuntos.xlsx'
+            },
+            {
+                'id': 3,
+                'path': r'C:\Dev\Scripts\Python\python-tools\excel\PCO_Referencias.xlsx'
+            }
         ]
     
     def update_lists(self):
         for row in self.list_to_update:
-            print(f'Acessando a planilha: {row}')
-            updateList = UpdateExcelList(row)
+            print(f'Acessando a planilha: {row['path']}')
+            updateList = UpdateExcelList(row['path'])
             updateList.updateList()
 
+    def read_bases(self):
+        for row in self.list_to_update:
+            if(row['id'] == 1):
+                print(f'Coletando dados da planilha: {row['path']}')
+                self.managers_list = pd.read_excel(row['path'])
+            elif(row['id'] == 2):
+                print(f'Coletando dados da planilha: {row['path']}')
+                self.budget_sets = pd.read_excel(row['path'])
+            elif(row['id'] == 3):
+                print(f'Coletando dados da planilha: {row['path']}')
+                self.references = pd.read_excel(row['path'])
+
     def run(self):
-        self.update_lists()
+        #self.update_lists()
+        self.read_bases()
